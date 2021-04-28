2096663a4969db4f59adf6b317482415c0ae913f
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-04-28 15:30:03 +0530

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -5,18 +5,17 @@ const ascii = require("ascii-table");
 // Create a new Ascii table
 let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
-
 module.exports = (client) => {
     // Read every commands subfolder
-    readdirSync("./commands/").forEach(dir => {
+    readdirSync(__dirname.replace("\handlers", "\commands")).forEach(dir => {
         // Filter so we only have .js command files
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+        const commands = readdirSync(`${__dirname.replace("\handlers", "\commands")}/${dir}/`).filter(file => file.endsWith(".js"));
     
         // Loop over the commands, and add all of them to a collection
         // If there's no name found, prevent it from returning an error,
         // By using a cross in the table we made.
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+            let pull = require(`${__dirname.replace("\handlers", "\commands")}/${dir}/${file}`);
     
             if (pull.name) {
                 client.commands.set(pull.name, pull);
