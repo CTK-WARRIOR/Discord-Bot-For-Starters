3cd7c46a42c04d3b61cad8b0210bded6b0397546
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-04-28 15:30:24 +0530

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -9,12 +9,12 @@ table.setHeading("Events", "Load status");
 
 module.exports = (client) => {
 
-  const commands = readdirSync(`./events/`).filter(file => file.endsWith(".js"));
+  const commands = readdirSync(__dirname.replace("\handlers", "\events")).filter(file => file.endsWith(".js"));
 
   for (let file of commands) {
 
     try {
-    let pull = require(`../events/${file}`);
+    let pull = require(`${__dirname.replace("\handlers", "\events")}/${file}`);
 
     if (pull.event && typeof pull.event !== "string") {
       table.addRow(file, `❌ -> Property event should be string.`);
