8d02fc6fe01919b7edd87541feb99f4e83f59bc8
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-09-17 22:00:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Intents, Collection } = require("discord.js"),
 { token, prefix, color, ownerId } = require("./settings.json"),
-client = new Client( { intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES ] })
+client = new Client( { intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES ] })
 
 client.prefix_commands = new Collection();
 client.slash_commands = new Collection();
