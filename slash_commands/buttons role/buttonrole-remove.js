3841224c27bebc8de3d14d11b3baf090a0176fef
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-01-03 23:24:39 +0500

    Update buttonrole-remove.js

---
+++ slash_commands/buttons role/buttonrole-remove.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, CommandInteraction } = require('discord.js');
+const simply = require('simply-djs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('buttonrole-remove')
+    .setDescription('Removes a button role from the click button menu.')
+    .addChannelOption((option) => option.setName('channel').setDescription('The channel to remove the button role from.').setRequired(true))
+    .addStringOption((option) => option.setName('message').setDescription('The message id').setRequired(true))
+    .addRoleOption((option) => option.setName('role').setDescription('The role to remove from the button role.').setRequired(true)),
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    run: async (client, interaction) => {
+        simply.betterBtnRole(client, interaction, {
+            type: 'remove'
+        })
+    }
+}
