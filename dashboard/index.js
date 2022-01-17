44728bf993c20c4ce00558d6ea5742102ed7a840
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-01-17 23:27:09 +0500

    Update index.js

--- dashboard/index.js
+++ dashboard/index.js
@@ -56,7 +56,6 @@ module.exports = (client) => {
 
   app.engine("ejs", ejs.renderFile);
   app.set("view engine", "ejs");
-  app.set("views", path.join(__dirname, "./templates"));
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
