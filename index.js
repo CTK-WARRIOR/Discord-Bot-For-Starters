41b6da9f37cc24d4188309aef20a754eb126f20f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-01-19 22:44:33 +0500

    Fixes the music system

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const { Client, Collection, Intents } = require("discord.js");
 const { token, prefix, color, ownerId } = require("./settings.json");
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ],
-    ws: {
-        properties: {
-            "$browser": "Discord Android"
-        }
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+  ws: {
+    properties: {
+      $browser: "Discord Android",
     },
-    partials: [
-      'CHANNEL',
-      'MESSAGE',
-      'REACTION'
-    ]
+  },
+  partials: ["CHANNEL", "MESSAGE", "REACTION"],
 });
 
 client.prefix_commands = new Collection();
@@ -26,7 +22,18 @@ client.settings = { prefix, color, ownerId };
 for (let handler of ["slash_command", "prefix_command", "event"])
   require(`./handlers/${handler}`)(client);
 
-// Export client 
+const { Player } = require("discord-player");
+
+const player = new Player(client, {
+  ytdlOptions: {
+    quality: "highestaudio",
+    highWaterMark: 1 << 25,
+  },
+});
+
+module.exports = player;
+
+// Export client
 module.exports = client;
 
 // Login
--- slash_commands/music/play.js
+++ slash_commands/music/play.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { Client, CommandInteraction } = require("discord.js");
-const player = require("../../client/player.js");
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const player = require("../../index.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,17 +8,41 @@ module.exports = {
     .setDescription("Plays a song from YouTube.")
     .addStringOption((option) =>
       option
-        .setName("query")
+        .setName("sonngtitle")
+        .setDescription("Enter a song title to play it for you :)")
         .setRequired(true)
-        .setDescription("The song to play.")
     ),
-    /**
-     * 
-     * @param {Client} client 
-     * @param {CommandInteraction} interaction 
-     */
+  /**
+   *
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   */
   run: async (client, interaction) => {
-      const songTitle = interaction.options.getString("query");
-      
+    const songTitle = interaction.options.getString("songtitle");
+
+    if (!interaction.member.voice.channel)
+      return interaction.followUp({
+        content: "Please join a voice channel first!",
+      });
+
+    const searchResult = await player.search(songTitle, {
+      requestedBy: interaction.user,
+      searchEngine: QueryType.AUTO,
+    });
+
+    const queue = await player.createQueue(interaction.guild, {
+      metadata: interaction.channel,
+    });
+
+    if (!queue.connection)
+      await queue.connect(interaction.member.voice.channel);
+
+    interaction.followUp({ content: `Playing ${songTitle}` });
+
+    searchResult.playlist
+      ? queue.addTracks(searchResult.tracks)
+      : queue.addTrack(searchResult.tracks[0]);
+
+    if (!queue.playing) await queue.play();
   },
 };
